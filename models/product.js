5c4ac8582204e48e9258411df7b1e484af7dbcb5
tojukiap <85228438+tojukiap@users.noreply.github.com>
2021-07-12 20:55:00 +0800

    ecommerce app

---
+++ models/product.js
@@ -0,0 +1,36 @@
+const products = [
+    {
+      id: 1,
+      name: 'nokia 3310',
+      image:
+        'https://dl.airtable.com/.attachments/6ac7f7b55d505057317534722e5a9f03/9183491e/product-3.jpg',
+      price: 5000,
+      desc: `Dual SIM, 2MP CAMERA WITH FLASH, FM RADIO, DARK BLUE COLOUR.`,
+    },
+    {
+      id: 2,
+      name: 'infinix hot 10',
+      image:
+        'https://dl.airtable.com/.attachments/da5e17fd71f50578d525dd5f596e407e/d5e88ac8/product-2.jpg',
+      price: 61000,
+      desc: `Dual SIM, 16MP DUAL REAR CAMERA WITH FLASH,  FM RADIO, GRAY COLOUR.`,
+    },
+    {
+      id: 3,
+      name: 'tecno camon 15',
+      image:
+        'https://dl.airtable.com/.attachments/05ecddf7ac8d581ecc3f7922415e7907/a4242abc/product-1.jpeg',
+      price: 70000,
+      desc: `Dual SIM, 24MP TRIPLE CAMERA WITH FLASH, FM RADIO, BLACK COLOUR.`,
+    },
+    {
+      id: 4,
+      name: ' iphone 8',
+      image:
+        'https://dl.airtable.com/.attachments/3245c726ee77d73702ba8c3310639727/f000842b/product-5.jpg',
+      price: 100000,
+      desc: `Dual SIM, 32MP TRIPLE CAMERA WITH FLASH, FM RADIO, RED COLOUR.`,
+    },
+  ]
+
+  module.exports = {products}
